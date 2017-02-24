74d66d0275054168e93b652ca91fe2145995e904
Jakob <jakob.tepe@gmail.com>
2017-02-24 22:39:38 +0000

    adding options funcitonality added

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', []);
+/*global angular */var myApp = angular.module('myApp', []);
 myApp.controller("AppCtrl",["$scope","$http",function ($scope, $http){
 	var polls;
 	console.log("Hello world from controller");
@@ -7,5 +7,9 @@ myApp.controller("AppCtrl",["$scope","$http",function ($scope, $http){
 		console.log(result.data);
 		$scope.polls = polls;
 	});
-	
 }]);
+/*global $*/ $(document).ready(function(){
+		$("#add").on("click", function(){
+			$("#options").append('<div class="row"><div class="input-field col s4"><input id="options" type="text" placeholder="Voting App" name="options"><label for="options" class="active">Create Options</label></div></div>');
+		});
+});
